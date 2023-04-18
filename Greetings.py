5aa7d6ad0dcfcb656f0ff2f748732e0f9c0fc2fd
applego <applegorillappa@gmail.com>
2023-04-18 10:37:34 +0900

    refactor: use Ref

--- Greetings.py
+++ Greetings.py
@@ -1,27 +1,32 @@
 import flet
-from flet import Column, Container, ElevatedButton, Page, Row, Text, UserControl, border_radius, colors, TextField
+from flet import Column, Container, ElevatedButton, Page, Row, Text, UserControl, border_radius, colors, TextField, Ref
 
 
 class GreetingsApp(UserControl):
     def build(self):
-        first_name = TextField(label="First name", autofocus=True)
-        last_name = TextField(label="Last name")
-        greetings = Column()
+        first_name = Ref[TextField]()
+        last_name = Ref[TextField]()
+        greetings = Ref[Column]()
 
         def btn_click(e):
-            greetings.controls.append(Text(f"Hello, {first_name.value} {last_name.value}!"))
-            first_name.value = ""
-            last_name.value = ""
+            greetings.current.controls.append(Text(f"Hello, {first_name.current.value} {last_name.current.value}!"))
+            first_name.current.value = ""
+            last_name.current.value = ""
             self.update()
-            first_name.focus()
+            first_name.current.focus()
 
         return Container(
             content=Column(
                 controls=[
-                    first_name,
-                    last_name,
+                    # first_name,
+                    # last_name,
+                    # この書き方だとfirst_nameやlast_nameが何なのかわからない→Ref（reactを参考）
+                    TextField(ref=first_name, label="First name", autofocus=True),
+                    TextField(ref=last_name, label="Last name"),
                     ElevatedButton(text="Say hello!", on_click=btn_click),
-                    greetings,
+                    # この書き方だとfirst_nameやlast_nameが何なのかわからない→Ref（reactを参考）
+                    # greetings,
+                    Column(ref=greetings),
                 ],
             )
         )
